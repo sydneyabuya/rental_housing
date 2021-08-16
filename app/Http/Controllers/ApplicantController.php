93f080ce85f86c1fbdaff847f1dac37c1d3963c4
Not-Caleb <calebngugi12@gmail.com>
2021-08-16 11:34:15 +0300

    returning removed files

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Applicant;
+
+class ApplicantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $applicant = Applicant::all();
+
+        return view('applicant.index', compact('applicant'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('applicant.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Applicant::create($request->all());
+
+        return redirect()->route('applicant.index')->with('success', 'Applicant created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Applicant $applicant)
+    {
+        return view('applicant.show', compact('applicant'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Applicant $applicant)
+    {
+        return view('applicant.edit', compact('applicant'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Applicant $applicant)
+    {
+        $applicant->update($request->all());
+
+        return redirect()->route('applicant.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Applicant $applicant)
+    {
+        $applicant->delete();
+
+        return redirect()->route('applicant.index');
+    }
+}
