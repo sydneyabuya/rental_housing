9551c0d4d068330932a52710fcd7ec26478ead9c
SydneySmoothvile <sidneybradleyabuya@gmail.com>
2021-08-21 08:53:01 +0300

    Basic Views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,16 +19,28 @@
                     </x-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
-                        {{ __('Tasks') }}
-                    </x-nav-link>
-                </div>
-
                 @if (Auth::user()->hasRole('tenant'))
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('applicant.create')" :active="request()->routeIs('applicant.*')">
                         {{ __('Applicants') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('applicant.create')" :active="request()->routeIs('applicant.*')">
+                        {{ __('Payment') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('applicant.create')" :active="request()->routeIs('applicant.*')">
+                        {{ __('Requests') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('applicant.create')" :active="request()->routeIs('applicant.*')">
+                        {{ __('Contact Us') }}
                     </x-nav-link>
                 </div>
                 @endif
@@ -119,6 +131,12 @@
                     </x-nav-link>
                 </div>
                 @endif
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
+                        {{ __('Tasks') }}
+                    </x-nav-link>
+                </div>
                 
             </div>
 
