e03a8c9e6a078e5c615a7b141fe2d1ba43ded583
Not-Caleb <calebngugi12@gmail.com>
2021-08-31 07:49:50 +0300

    fixed tawkto account

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -49,7 +49,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/612c3c0bd6e7610a49b29def/1feacl3n4';
+s1.src='https://embed.tawk.to/612db375d6e7610a49b2d1a1/1fed8a23h';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
